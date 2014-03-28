72ad0a1671b3b57c37b53167c7b4499815318f3a
Daniel Farrell <dfarrell07@gmail.com>
2014-03-28 00:53:39 -0400

    Added pip, virtualenv and virtualenvwrapper to Fedora install

--- linux_setup.sh
+++ linux_setup.sh
@@ -133,7 +133,9 @@ fedora_packages()
     sudo yum update
     sudo yum install git tmux wget vim-X11 vim ipython nmap irssi nload mtr i3 \
                      i3status zsh irssi google-chrome-stable scrot \
-                     network-manager-applet xbacklight vlc
+                     network-manager-applet xbacklight vlc python-virtualenv \
+                     python-pip
+    sudo pip install virtualenvwrapper
 }
 
 ubuntu_packages()
