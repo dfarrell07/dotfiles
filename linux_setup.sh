177c1d31a9d8d39acfd3a8df6fbdbd975a7f8478
Daniel Farrell <dfarrell07@gmail.com>
2014-05-12 13:19:24 -0400

    Remove dupe irssi package install

--- linux_setup.sh
+++ linux_setup.sh
@@ -146,7 +146,7 @@ fedora_packages()
     add_chrome_repo
     add_vlc_repo
     sudo yum update
-    sudo yum install git tmux wget vim-X11 vim ipython nmap irssi nload mtr i3 \
+    sudo yum install git tmux wget vim-X11 vim ipython nmap nload mtr i3 \
                      i3status zsh irssi google-chrome-stable scrot \
                      network-manager-applet xbacklight vlc python-virtualenv \
                      python-pip openssl-devel zlib-devel ncurses-devel \
