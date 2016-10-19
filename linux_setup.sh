84bbf0f98b39f10a46e44ac8f953ad8d6cee6052
Daniel Farrell <dfarrell@redhat.com>
2016-10-18 21:21:04 -0400

    Install pkg to get kinit for TGT gen
    Used for logging into RH Brew web UI
    
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- linux_setup.sh
+++ linux_setup.sh
@@ -356,7 +356,7 @@ fedora_packages()
                      ShellCheck libvirt libvirt-devel \
                      htop nload kernel-devel \
                      dkms rubygem-bundler koji ansible redhat-rpm-config \
-                     python-devel libcurl-devel fuse-exfat iotop
+                     python-devel libcurl-devel fuse-exfat iotop krb5-workstation
     sudo dnf groupinstall -y "C Development Tools and Libraries"
     sudo pip install --upgrade pip
     sudo pip install virtualenvwrapper tox virtualenv --upgrade
