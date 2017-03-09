8a9aba9a95e3f8aff5a4e59f8279e5829ec0b5e8
Daniel Farrell <dfarrell@redhat.com>
2017-03-09 16:15:11 -0500

    Make sure external displays are on in fix_displays
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- fix_displays.sh
+++ fix_displays.sh
@@ -1,7 +1,11 @@
 #/usr/bin/env sh
 
+# Make sure external dispays are on, may not be after docking w/o reboot
+xrandr --output DP2-2 --mode 1920x1200
+xrandr --output DP2-3 --mode 1920x1200
+
+# Put the displays in positions that make sense in physical workspace
 xrandr --output eDP1 --right-of DP2-2
 xrandr --output DP2-3 --left-of DP2-2
-
 xrandr --output DP2-3 --rotate right
 xrandr --output DP2-2 --rotate right
